428b2e61a72f15507756fc210506423ddebbcecc
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-30 16:23:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,5 +4,11 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-   
+  
+def position_taken? (board, index)
+ if board[index] == " " || board[index]=="" || board[index]== nil
+ return false  
+ else
+   return true
+ end 
  end
