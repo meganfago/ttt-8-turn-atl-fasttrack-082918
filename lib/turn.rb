bba9096e400a41a2f9ae219cb7a95648bf4119b4
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-30 16:37:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,10 @@ def position_taken? (board, index)
  end 
  
  def valid_move(board, index)
-   
-   return true
+  if board[index] == " " || board[index]=="" || board[index]== nil
+ return false  
+ else
+   return true  
+ 
  end
    
